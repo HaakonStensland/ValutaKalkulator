f4e21f4f114611260994019053f2094feb30af32
OPDADDY <122084297+OPDADDY@users.noreply.github.com>
2023-09-22 11:17:41 +0200

    Endret på koden i javascript og gjorde den mer riktig

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const apiKey = 'ec25136b81468ccf1a841ecd'; // API koden jeg fikk av nettsiden
 const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
-const amountInput = document.getElementById('amount'); // Denne koden linker opp med htmlen og får teksten til og faktis ha en betydning
+const amountInput = document.getElementById('amount');
 const currencySelect = document.getElementById('currency');
-const convertButton = document.getElementById('convert'); // dette er buttonen du trykker for å converte pengene det koden her gjør her gjør at den gjør noe mer en å bare være en button
+const convertButton = document.getElementById('convert');
+const ChangeButton = document.getElementById('Change');
 const resultSpan = document.getElementById('result');
+let data; // Declare a variable to store the fetched data
 
 // Fetch and populate currency options
 fetch(apiUrl)
     .then(response => response.json())
-    .then(data => {
+    .then(responseData => {
+        data = responseData; // Store the fetched data
         const currencies = Object.keys(data.conversion_rates);
         currencies.forEach(currency => {
             const option = document.createElement('option');
@@ -16,25 +19,29 @@ fetch(apiUrl)
             option.value = currency;
             currencySelect.appendChild(option);
         });
+    })
+    .catch(error => {
+        console.error(error);
+        resultSpan.textContent = 'Error fetching currency data. Please try again later.';
     });
 
-// Convert currency
+// Event listener for the "Convert" button
 convertButton.addEventListener('click', () => {
     const amount = parseFloat(amountInput.value);
     const selectedCurrency = currencySelect.value;
 
     if (!isNaN(amount)) {
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                const exchangeRate = data.conversion_rates[selectedCurrency];
+        if (data) {
+            const exchangeRate = data.conversion_rates[selectedCurrency];
+            if (exchangeRate) {
                 const convertedAmount = (amount * exchangeRate).toFixed(2);
                 resultSpan.textContent = `${amount} USD = ${convertedAmount} ${selectedCurrency}`;
-            })
-            .catch(error => {
-                console.error(error);
-                resultSpan.textContent = 'Error fetching data. Please try again later.';
-            });
+            } else {
+                resultSpan.textContent = 'Selected currency rate not available.';
+            }
+        } else {
+            resultSpan.textContent = 'Currency data not available. Please try again later.';
+        }
     } else {
         resultSpan.textContent = 'Please enter a valid amount.';
     }
