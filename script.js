5556f65aa6853e5efb9a50e219f4c2f72099e68b
OPDADDY <122084297+OPDADDY@users.noreply.github.com>
2023-09-26 09:32:47 +0200

    glemte og pushe for en stund siden men her er den ferdig jeg skal legge til litt til

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-const apiKey = 'ec25136b81468ccf1a841ecd'; // API koden jeg fikk av nettsiden
-const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
-const amountInput = document.getElementById('amount');
-const currencySelect = document.getElementById('currency');
-const changeSelect = document.getElementById('changeSelect');
-const resultSpan = document.getElementById('result');
+  const apiKey = 'ec25136b81468ccf1a841ecd'; // Replace with your API key
+        const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
+        const amountInput = document.getElementById('amount');
+        const fromSelect = document.getElementById('from');
+        const toSelect = document.getElementById('to');
+        const convertButton = document.getElementById('convert');
+        const resultSpan = document.getElementById('result');
+        let data;
 
-let data;
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(responseData => {
+                data = responseData;
+                const currencies = Object.keys(data.conversion_rates);
 
-function updateConversion() {
-    const amount = parseFloat(amountInput.value);
-    const selectedChangeCurrency = changeSelect.value;
-    const exchangeRate = data.conversion_rates[selectedChangeCurrency];
+                currencies.forEach(currency => {
+                    fromSelect.appendChild(new Option(currency, currency));
+                    toSelect.appendChild(new Option(currency, currency));
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
 
-    if (!isNaN(amount) && data && exchangeRate) {
-        const convertedAmount = (amount / exchangeRate).toFixed(2);
-        resultSpan.textContent = `${amount} ${currencySelect.value} = ${convertedAmount} ${selectedChangeCurrency}`;
-    } else {
-        resultSpan.textContent = 'Conversion not possible. Please check your inputs.';
-    }
-}
 
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(responseData => {
-        data = responseData;
-        const currencies = Object.keys(data.conversion_rates);
-        currencies.forEach(currency => {
-            currencySelect.appendChild(new Option(currency, currency));
-            changeSelect.appendChild(new Option(currency, currency));
-        });
+        function convertCurrency() {
+            const fromCurrency = fromSelect.value;
+            const toCurrency = toSelect.value;
+            const amount = parseFloat(amountInput.value);
 
-        changeSelect.addEventListener('change', updateConversion);
-        updateConversion();
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-        resultSpan.textContent = 'Error fetching currency data. Please try again later.';
-    });
+            if (!isNaN(amount) && data.conversion_rates[fromCurrency] && data.conversion_rates[toCurrency]) {
+                const exchangeRate = data.conversion_rates[toCurrency] / data.conversion_rates[fromCurrency];
+                const convertedAmount = (amount * exchangeRate).toFixed(2);
+                resultSpan.textContent = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+            } else {
+                resultSpan.textContent = 'Conversion not possible. Please check your inputs.';
+            }
+        }
+
+        convertButton.addEventListener('click', convertCurrency);
