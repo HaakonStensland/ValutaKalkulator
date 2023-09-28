f5beede007622de5de894aca600a15ea098668a8
OPDADDY <122084297+OPDADDY@users.noreply.github.com>
2023-09-28 10:35:03 +0200

    La til en history box som skal bli redigert litt

--- script.js
+++ script.js
@@ -1,40 +1,62 @@
-  const apiKey = 'ec25136b81468ccf1a841ecd'; // Replace with your API key
-        const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`; //API nettsiden som jeg tok den api koden
-        const amountInput = document.getElementById('amount'); // Det er den som fokuserer på hvor mye som blir puttet in også skal den kalkulere det 
-        const fromSelect = document.getElementById('from'); // Den heter valutaen fra nettsiden om jeg ikke tar helt feil 
-        const toSelect = document.getElementById('to'); // denne skal gjøre akkurat det samme som den over ^^
-        const convertButton = document.getElementById('convert'); // dette linker buttonen i html koden til javascript koden her sånn at den converter det den fikk beskjed om her som er og gå på api koden nettsiden og converte den informasjonen.
-        const resultSpan = document.getElementById('result'); // Den er linket opp med resultSpan.textcontent som gir deg enten et svar eller hvis noe failer så får du meldingen som står "noe skjedde. Sjekk inputsa dine"
-        let data;
+const apiKey = 'ec25136b81468ccf1a841ecd'; // Replace with your API key
+const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
+const amountInput = document.getElementById('amount'); 
+const fromSelect = document.getElementById('from');
+const toSelect = document.getElementById('to');
+const convertButton = document.getElementById('convert');
+const resultSpan = document.getElementById('result');
+const historyList = document.getElementById('history'); // Add this line
 
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(responseData => {
-                data = responseData;
-                const currencies = Object.keys(data.conversion_rates);
+let data;
+let conversionHistory = []; // Add this array to store conversion history
 
-                currencies.forEach(currency => {
-                    fromSelect.appendChild(new Option(currency, currency));
-                    toSelect.appendChild(new Option(currency, currency));
-                });
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(responseData => {
+        data = responseData;
+        const currencies = Object.keys(data.conversion_rates);
 
+        currencies.forEach(currency => {
+            fromSelect.appendChild(new Option(currency, currency));
+            toSelect.appendChild(new Option(currency, currency));
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 
-        function convertCurrency() {
-            const fromCurrency = fromSelect.value;
-            const toCurrency = toSelect.value;
-            const amount = parseFloat(amountInput.value);
+function convertCurrency() {
+    const fromCurrency = fromSelect.value;
+    const toCurrency = toSelect.value;
+    const amount = parseFloat(amountInput.value);
 
-            if (!isNaN(amount) && data.conversion_rates[fromCurrency] && data.conversion_rates[toCurrency]) {
-                const exchangeRate = data.conversion_rates[toCurrency] / data.conversion_rates[fromCurrency];
-                const convertedAmount = (amount * exchangeRate).toFixed(2);
-                resultSpan.textContent = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
-            } else {
-                resultSpan.textContent = 'Noe har skjedd. Sjekk koden din.';
-            }
-        }
+    if (!isNaN(amount) && data.conversion_rates[fromCurrency] && data.conversion_rates[toCurrency]) {
+        const exchangeRate = data.conversion_rates[toCurrency] / data.conversion_rates[fromCurrency];
+        const convertedAmount = (amount * exchangeRate).toFixed(2);
+        resultSpan.textContent = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+        
+        // Store the conversion in history
+        const conversionText = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+        addToHistory(conversionText);
+    } else {
+        resultSpan.textContent = 'Noe har skjedd. Sjekk koden din.';
+    }
+}
 
-        convertButton.addEventListener('click', convertCurrency);
+function addToHistory(conversionText) {
+    conversionHistory.push(conversionText); // Add the conversion to the history array
+    updateHistoryList(); // Update the history list
+}
+
+function updateHistoryList() {
+    historyList.innerHTML = ''; // Clear the history list
+
+    // Loop through the conversion history and create list items
+    conversionHistory.forEach((conversion, index) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `Conversion ${index + 1}: ${conversion}`;
+        historyList.appendChild(listItem);
+    });
+}
+
+convertButton.addEventListener('click', convertCurrency);
